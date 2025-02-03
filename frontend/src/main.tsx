6c3f8aec86ad2f2b9e9a5b9a0c5bbb75fdd3e8e1
Federico <fedemont2006@gmail.com>
2025-02-03 21:30:09 +0100

    Add React-Router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter,RouterProvider } from "react-router-dom"
+
+import LoginPage from "./pages/login"
+
 import './index.css'
-import App from './App.tsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage/>
+  }
+]);
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
