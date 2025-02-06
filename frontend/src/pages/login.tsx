90a8a6d5589eda6cd62de834f03b7b26837164e8
Federico <fedemont2006@gmail.com>
2025-02-06 21:26:49 +0100

    Login Input

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,10 +1,16 @@
 import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input"
+
+
 export default function LoginPage() {
   return (
     <>
       <div className="h-36"/>
       <div className="flex items-center justify-center">        
         <Button size={"login"} variant={"login"}>Sign in</Button>
+      </div>
+      <div className="flex items-center justify-center m-10">
+      <Input placeholder="Email"></Input>
       </div>
     </>
   );
