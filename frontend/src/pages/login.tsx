ad12500e21044e1df80e993acc82d53d9dd76438
Federico <fedemont2006@gmail.com>
2025-02-08 11:08:55 +0100

    Ipunt_pass

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,5 +1,6 @@
 import { Button } from "../components/ui/button";
-import { Input } from "../components/ui/input"
+import { Input as Input_email}   from "../components/ui/input_email"
+import { Input as Input_pass}   from "../components/ui/input_pass"
 
 
 export default function LoginPage() {
@@ -10,7 +11,8 @@ export default function LoginPage() {
         <Button size={"login"} variant={"login"}>Sign in</Button>
       </div>
       <div className="flex items-center justify-center m-10">
-      <Input placeholder="Email"></Input>
+      <Input_pass placeholder="Password" ></Input_pass>
+      
       </div>
     </>
   );
