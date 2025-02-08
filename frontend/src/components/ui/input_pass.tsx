c1d64118d751e9a871f507c2ba93a1baddef6022
Federico <fedemont2006@gmail.com>
2025-02-08 14:57:23 +0100

    Change state Input_pass

--- frontend/src/components/ui/input_pass.tsx
+++ frontend/src/components/ui/input_pass.tsx
@@ -1,22 +1,32 @@
 import * as React from "react";
 import { Eye, EyeSlash } from "@phosphor-icons/react";
+import { useState } from "react";
 
 import { cn } from "@/lib/utils";
 
+  
+
+
+
 const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
   ({ className, ...props }, ref) => {
-    return (
+    const [state,setState]=useState(false);
+    return (      
       <div className="relative w-3/4">
         <input
-          
+          type={ state ? "text" : "password"}
           className={cn(
-            "h-9 w-full rounded-md border border-login_colors-place_holder bg-transparent px-3 pr-10 py-1 text-login_colors-button_bg/text shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+            "h-9 w-full rounded-md border border-login_colors-place_holder bg-transparent px-3 pr-12 py-1 text-login_colors-button_bg/text shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
             className
           )}
           ref={ref}
           {...props}
         />
-        <EyeSlash className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer" />
+        
+        {state ? <Eye id="Eye" className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"  onClick={()=>setState(!state)}/> 
+        : <EyeSlash id="EyeSlash" className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"  onClick={()=>setState(!state)} />}
+        
+        
       </div>
     );
   }
