7f5017ac463cb580a7929490b5f4b742355d235b
Federico <fedemont2006@gmail.com>
2025-02-13 15:58:04 +0100

    Add Button mute all

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,7 +1,9 @@
-import { Button } from "../components/ui/button";
+import { Button } from "../components/ui/button_sign";
 import { Input as Input_email}   from "../components/ui/input_email"
 import { Input as Input_pass}   from "../components/ui/input_pass"
 import { Badge } from "../components/ui/badge"
+import { Button as Button_channels } from "../components/ui/button_channels"
+import {Button as Button_mute} from "../components/ui/button"
 
 
 export default function LoginPage() {
@@ -15,8 +17,13 @@ export default function LoginPage() {
       <Input_pass placeholder="Password" ></Input_pass>      
       </div>
       <div className="flex items-center justify-center m-10">
-      <Badge variant={"output_input"}>INPUT</Badge>
+      <Button_channels variant={"channels_activated"}>CH2</Button_channels>
+      <Button_channels variant={"channels_disabled"}>CH2</Button_channels>
       </div>
+      <div className="flex items-center justify-center m-10">
+         <Button_mute variant="modified">MUTE ALL</Button_mute>
+      </div>
+
     </>
   );
 }
