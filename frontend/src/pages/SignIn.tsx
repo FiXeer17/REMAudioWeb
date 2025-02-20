a4649147700101cee9abbba87ad9ce21e88b961c
Federico <fedemont2006@gmail.com>
2025-02-20 16:00:33 +0100

    Work on Login page

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,25 +1,29 @@
+import { Link } from "react-router-dom";
 import { Button as Button_sign } from "../components/ui/button_sign";
 import { Input as Input_email } from "../components/ui/input_email";
 import { Input as Input_pass } from "../components/ui/input_pass";
-import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
-
-
+import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 
 export default function SignInPage() {
   return (
-    <>
-    <div className="grid grid-rows-5 h-screen">
-      <div className="">Riga 1</div>
-      <div className="">
-        <Avatar>
-          <AvatarImage src="/User_avatar.svg"/>
+    <div className="grid grid-rows-6 min-h-svh">
+      <div></div>
+      <div className="flex justify-center">
+        <Avatar className="w-3/5">
+          <AvatarImage src="/REM_avatar.svg" />
         </Avatar>
       </div>
-      <div className="">
+      <div className="flex flex-col items-center justify-center gap-[30%] row-span-2">
+        <Input_email placeholder="Email" />
+        <div>
+          <Input_pass placeholder="Password"/>
+          <h1 className="text-white self-start">Forgot password?</h1>
+        </div>
       </div>
-      <div className="">Riga 4</div>
-      <div className="">Riga 5</div>
+      <div className="flex justify-center">
+        <Button_sign variant={"login"} size={"login"}>Sign In</Button_sign>
+        <link rel="Register"></link>
+      </div>
     </div>
-    </>
   );
 }
