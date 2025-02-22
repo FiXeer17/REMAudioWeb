9ad8e44c196bf510502e9aebdf14360989850132
Federico <fedemont2006@gmail.com>
2025-02-22 12:11:42 +0100

    Add Forgot password

--- frontend/src/components/ui/input_pass.tsx
+++ frontend/src/components/ui/input_pass.tsx
@@ -8,25 +8,28 @@ import { cn } from "@/lib/utils";
 
 
 
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input"> & {Forgot:String}>(
+  ({ className,Forgot,...props }, ref) => {
     const [state,setState]=useState(false);
+
     return (      
-      <div className="relative w-3/4">
-        <input
-          type={ state ? "text" : "password"}
-          className={cn(
-            "h-9 w-full rounded-md border border-login_colors-place_holder bg-transparent px-3 pr-12 py-1 text-login_colors-button_bg/text shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-            className
-          )}
-          ref={ref}
-          {...props}
-        />
-        
-        {state ? <Eye id="Eye" className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"  onClick={()=>setState(!state)}/> 
-        : <EyeSlash id="EyeSlash" className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"  onClick={()=>setState(!state)} />}
-        
-        
+      <div className="flex flex-col items-center justify-center relative w-3/4">
+        <div className="flex flex-col relative w-full   ">
+          <input
+            type={ state ? "text" : "password"}
+            className={cn(
+              "h-9 w-full rounded-md border border-login_colors-place_holder bg-transparent px-3 pr-12 py-1 text-login_colors-button_bg/text shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+              className
+            )}
+            ref={ref}
+            {...props}
+          />
+          
+          {state ? <Eye id="Eye" className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"  onClick={()=>setState(!state)}/> 
+          : <EyeSlash id="EyeSlash" className="absolute right-3 top-1/2 transform -translate-y-1/2 text-white cursor-pointer"  onClick={()=>setState(!state)} />}
+                  
+        </div>
+        <h1 className= {cn("flex self-start text-white text-sm mt-1 ",Forgot)}>Forgot password?</h1>
       </div>
     );
   }
