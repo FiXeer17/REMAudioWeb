f2c1592d3a78c0d077933adaa97474c5eeca16ab
I3elle <supertizionico@gmail.com>
2025-02-23 18:30:12 +0100

    removed useful output of db_url

--- backend/src/db.rs
+++ backend/src/db.rs
@@ -3,7 +3,6 @@ use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 pub async fn establish_connection() -> Pool<Postgres> {
     let db_url = Env::get_db_url();
-    println!("{db_url}");
     PgPoolOptions::new()
         .max_connections(5)
         .connect(&db_url)
