eb4922d64f0f61a6218c33758e14840e2552f9e9
Federico <fedemont2006@gmail.com>
2025-02-24 23:27:10 +0100

    Add Form

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter,RouterProvider } from "react-router-dom"
+import { createBrowserRouter,Navigate,RouterProvider } from "react-router-dom"
 
 import SignIn from "./pages/SignIn"
 import Register from "./pages/Register"
 
 
 const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Navigate to="/login" replace/>
+  },
   {
     path: "/login",
     element: <SignIn/> 
