1719dd61a54cfd07a08dfa2a74ca06224b06b91e
Federico <fedemont2006@gmail.com>
2025-02-25 17:03:02 +0100

    Getting login data

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -3,6 +3,7 @@ import { Button as Button_sign } from "../components/ui/button_sign";
 import { Input as Input_email } from "../components/ui/input_email";
 import { Input as Input_pass } from "../components/ui/input_pass";
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
+import axios from "axios";
 
 export default function SignInPage() {
   return (
@@ -13,10 +14,10 @@ export default function SignInPage() {
           <AvatarImage className="w-4/5" src="/REM_avatar.svg" />
         </Avatar>
       </div>
-      <Form className="flex flex-col row-span-4 justify-center gap-[10%]">
+      <Form method="post" action="/login" className="flex flex-col row-span-4 justify-center gap-[10%]">
         <div className="flex flex-col items-center justify-center gap-8">
-          <Input_email placeholder="Email" />
-          <Input_pass className="visible" Eye_state={"visible"} Forgot={"visible"} placeholder="Password" />
+          <Input_email placeholder="Email" name="email"/>
+          <Input_pass className="visible" Eye_state={"visible"} Forgot={"visible"} placeholder="Password" name="password" />
         </div>
         <div className="flex flex-col items-center justify-start mt-8 ">
           <Button_sign variant={"login"} size={"login"} type="submit">
@@ -30,4 +31,14 @@ export default function SignInPage() {
         
     </div>
   );
+}
+export const loginAction = async({ request }: {request:Request})=>{
+  const data= await request.formData()
+  const sub={
+    email: data.get('email'),
+    password: data.get('password'),
+    session_type:"web"
+
+  }
+  console.log(sub)
 }
