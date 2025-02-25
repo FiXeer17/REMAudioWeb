6c2254df75572624f5eb5eb9a721e9ae7f0d5db8
I3elle <supertizionico@gmail.com>
2025-02-25 11:52:51 +0100

    fix: token expiring when created, claims struct is public

--- backend/src/hasher.rs
+++ backend/src/hasher.rs
@@ -4,14 +4,14 @@ use argon2::{
     Argon2, PasswordHash, PasswordVerifier,
 };
 use chrono::Utc;
-use jsonwebtoken::{encode, EncodingKey, Header};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, TokenData, Validation};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-struct Claims {
-    sub: i32,
-    session_type: String,
-    exp: usize,
+pub struct Claims {
+    pub sub: i32,
+    pub session_type: String,
+    pub exp: usize,
 }
 
 pub fn argon2_enc(password: &str) -> Result<String, Error> {
@@ -41,8 +41,8 @@ pub fn id_to_jwt(id: i32, session_type: String) -> Result<String, Box<dyn std::e
     let default_jwt_duration: i64 = 3; // hours
 
     let jwt_exp = Utc::now()
-        .checked_sub_signed(chrono::Duration::hours(default_jwt_duration))
-        .expect("failed to create a jwt_exp.")
+        .checked_add_signed(chrono::Duration::hours(default_jwt_duration))
+        .unwrap()
         .timestamp();
 
     let jwt_secret = Env::get_jwt_secret();
@@ -58,4 +58,14 @@ pub fn id_to_jwt(id: i32, session_type: String) -> Result<String, Box<dyn std::e
         &EncodingKey::from_secret(jwt_secret.as_bytes()),
     )?;
     Ok(token)
+}
+
+pub fn jwt_to_id(jwt_token: String) -> Result<i32, jsonwebtoken::errors::Error> {
+    let jwt_secret = Env::get_jwt_secret();
+    let claim = decode::<Claims>(
+        &jwt_token,
+        &DecodingKey::from_secret(jwt_secret.as_bytes()),
+        &Validation::new(jsonwebtoken::Algorithm::HS256),
+    )?;
+    Ok(claim.claims.sub)
 }
