9fffbb1741bb0ef4acf830e8011240bcc7c3ba4c
I3elle <supertizionico@gmail.com>
2025-02-25 11:51:13 +0100

    formatted

--- backend/src/env_dns.rs
+++ backend/src/env_dns.rs
@@ -2,7 +2,7 @@ use dotenv::{dotenv, from_filename};
 
 pub const DATABASE_URL: &str = "DATABASE_URL";
 pub const JWT_SECRET: &str = "JWT_SECRET";
-pub const DATABASE_NAME : &str = "DATABASE_NAME";
+pub const DATABASE_NAME: &str = "DATABASE_NAME";
 
 pub struct Env {
     database_url: String,
@@ -22,7 +22,7 @@ impl Env {
         Env {
             database_url,
             jwt_secret,
-            database_name
+            database_name,
         }
     }
 
@@ -32,7 +32,7 @@ impl Env {
     pub fn get_jwt_secret() -> String {
         Env::get_vars().jwt_secret
     }
-    pub fn get_db_name() -> String{
+    pub fn get_db_name() -> String {
         Env::get_vars().database_name
     }
 }
