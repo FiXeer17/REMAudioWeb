09957ae524c5e6afa9666a53e9e467d0057f72f5
Federico <fedemont2006@gmail.com>
2025-03-02 14:50:00 +0100

    Add register page contorl

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,34 +1,29 @@
 import { ArrowLeft } from "@phosphor-icons/react";
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
-import { Link,Form, redirect } from "react-router-dom";
+import { Link,redirect } from "react-router-dom";
 import { Button as Button_register } from "../components/ui/button_sign";
 import { Input as Input_email } from "../components/ui/input_email";
 import { Input as Input_pass } from "../components/ui/input_pass";
 import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form"
+import { watch } from "fs";
+
+type FormFields = {
+  username:string;
+  email:string;
+  password:string;
+  confirm_password:string;
+}
 
 export default function Register() {
-  const [match,setMatch]=useState(false)
-
-  const credentialValidation= async (data:FormData)=>{
-    try{
-    const username = data.get('username') 
-    const email = data.get('email') 
-    const password = data.get('password')
-    const confirm_password= data.get('confirm_password') 
-    const session_type = "web" 
-    console.log(username)
-    console.log(password)
-    if (password===confirm_password)
-      setMatch(true)
-    else{
-      setMatch(false)
-    }
-
+  const { register, handleSubmit,setError,watch,formState: { errors } } = useForm<FormFields>();
+  
+  const onSubmit: SubmitHandler<FormFields> =  async (data)=>{
+    
   }
-  catch(error){
-    return redirect("/register")
-    }
-  }
+
+  const password=watch("password")
+
   return (
     <div className="grid grid-rows-6 min-h-svh">
       <div className="mt-9 ml-7">
@@ -41,15 +36,40 @@ export default function Register() {
           <AvatarImage className="w-4/5" src="/REM_avatar.svg" />
         </Avatar>
       </div>
-      <Form  className="flex flex-col row-span-4 justify-center gap-[10%]">
+      <form  className="flex flex-col row-span-4 justify-center gap-[10%]" onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-col items-center gap-2">
-        <Input_email placeholder="Username" name="username"/>
-        <Input_email placeholder="Email" name="email"/>
+        <Input_email {...register("username",{
+          required: "Username required"
+          })} placeholder="Username"/>
+        <Input_email {...register("email",{
+          required: "Email required",
+          pattern: {
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message: "Email format not valid"
+          }
+        })} placeholder="Email"/>
       </div>
       <div className="flex flex-col items-center gap-2 ">
-          <Input_pass className="visible" Eye_state={"visible"} Forgot={"hidden"} placeholder="Password" name="password"/>
-          <Input_pass className="visible" Eye_state={"hidden"} Forgot={"hidden"} placeholder="Confirm password" name="confirm_password"/>
-          {match && <h1 className="text-white" >Password doesn't match</h1>}
+          <Input_pass {...register("password",{
+            required: "Password required",
+            pattern: {
+              value: /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/,
+              message: "Password must be at least 8 characters long, with an uppercase letter, a number, and a special character"
+                }
+ 
+          })} className="visible" Eye_state={"visible"} Forgot={"hidden"} placeholder="Password"/>
+          {errors.password && (<h1 className="text-white">{errors.password.message}</h1>)}
+          <Input_pass {...register("confirm_password",{
+          required: "Must confirm the password",
+          validate: (value) => {
+            if(value != password){
+              return  "Password doesn't match"
+            }
+            return true
+          }
+            })} className="visible" Eye_state={"hidden"} Forgot={"hidden"} placeholder="Confirm password"/>
+          
+          {errors.confirm_password && (<h1 className="text-white">{errors.confirm_password.message}</h1>)}
       </div>
       <div className="flex flex-col items-center justify-start mt-7">
         <Button_register variant={"login"} size={"login"}>
@@ -57,7 +77,7 @@ export default function Register() {
         </Button_register>
         <Link to={"/Login"} className="text-login_colors-button_bg/text font-bold mt-5">Sign in</Link>
       </div>
-      </Form>
+      </form>
     </div>
   );
 }
