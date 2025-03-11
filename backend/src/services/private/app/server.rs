46903280259b9d74fb7b99044a74ddc7dc5e4ece
I3elle <supertizionico@gmail.com>
2025-03-11 11:59:04 +0100

    feat: WebSocket server to contai all WebSocket sessions

--- backend/src/services/private/app/server.rs
+++ backend/src/services/private/app/server.rs
@@ -1,38 +1,37 @@
-use actix::prelude::*;
-use std::collections::HashSet;
-use actix::Message;
 use super::session::WsSession;
+use actix::prelude::*;
+use actix::Message;
+use std::collections::HashSet;
 
-#[derive(Message,Debug)]
-#[rtype(result="()")]
-pub struct Connect{
-    pub addr: Addr<WsSession>
+#[derive(Message, Debug)]
+#[rtype(result = "()")]
+pub struct Connect {
+    pub addr: Addr<WsSession>,
 }
 
-#[derive(Message,Debug,Clone)]
-#[rtype(result="()")]
-pub struct BroadcastMessage{
-    pub message: String
+#[derive(Message, Debug, Clone)]
+#[rtype(result = "()")]
+pub struct BroadcastMessage {
+    pub message: String,
 }
 
-pub struct WsServer{
-    pub clients : HashSet<Addr<WsSession>>
+pub struct WsServer {
+    pub clients: HashSet<Addr<WsSession>>,
 }
 
-
-impl Actor for WsServer{
+impl Actor for WsServer {
     type Context = Context<Self>;
 }
 
-impl WsServer{
-    pub fn new() -> Self{
-        WsServer{
-            clients : HashSet::new()
+impl WsServer {
+    pub fn new() -> Self {
+        WsServer {
+            clients: HashSet::new(),
         }
     }
 }
 
-impl Handler<Connect> for WsServer{
+impl Handler<Connect> for WsServer {
     type Result = ();
     fn handle(&mut self, msg: Connect, _: &mut Self::Context) -> Self::Result {
         let address = msg.addr;
@@ -40,15 +39,17 @@ impl Handler<Connect> for WsServer{
 
         /*  TODO: let datas = read_all();
         for client in &self.clients{
-            
+
             let message = BroadcastMessage{
                 message:datas.clone()
             };
             client.do_send(message);
         } */
 
-       for client in &self.clients{
-            client.do_send(BroadcastMessage{message:"new".to_string()});
-       }
+        for client in &self.clients {
+            client.do_send(BroadcastMessage {
+                message: "new".to_string(),
+            });
+        }
     }
 }
