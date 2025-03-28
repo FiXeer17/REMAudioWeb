7b728e905d8d86403dc0dd4febf22439e81680b6
I3elle <supertizionico@gmail.com>
2025-03-28 11:06:00 +0100

    fix: only username is needed since email does not ehxsist anymore

--- backend/src/services/public/interfaces.rs
+++ backend/src/services/public/interfaces.rs
@@ -1,24 +1,24 @@
-use crate::services::public::register::schemas::ReturnCreateUser;
-use crate::services::public::signin::schemas::ReturnFullUser;
+use crate::utils::hasher::argon2_enc;
+use crate::{utils::configs::Env,services::public::signin::schemas::ReturnFullUser};
 use crate::AppState;
 use actix_web::web::Data;
 
-pub async fn check_email(pgpool: &Data<AppState>, email: &str) -> Result<bool, sqlx::Error> {
-    let query_string = "SELECT email FROM users WHERE email = $1 AND deleted_at IS NULL;";
+pub async fn check_username(pgpool: &AppState, username: &str) -> Result<bool, sqlx::Error> {
+    let query_string: &str = "SELECT username FROM users WHERE username = $1 AND deleted_at IS NULL;";
     let result = sqlx::query(query_string)
-        .bind(email.to_string())
+        .bind(username.to_string())
         .fetch_optional(&pgpool.db)
         .await?;
     Ok(result.is_some())
 }
 
-pub async fn from_email(
+pub async fn from_username(
     pgpool: &Data<AppState>,
-    email: &str,
+    username: &str,
 ) -> Result<ReturnFullUser, sqlx::Error> {
-    let query_string = "SELECT id,username,email,admin,password FROM users WHERE email = $1 ";
+    let query_string = "SELECT id,username,admin,password FROM users WHERE email = $1 ";
     match sqlx::query_as::<_, ReturnFullUser>(query_string)
-        .bind(email)
+        .bind(username)
         .fetch_optional(&pgpool.db)
         .await?
     {
@@ -40,26 +40,37 @@ pub async fn from_id(pgpool: &Data<AppState>, id: i32) -> Result<ReturnFullUser,
     }
 }
 
-pub async fn insert_user(
-    pgpool: Data<AppState>,
-    username: &str,
-    email: &str,
-    password: &str,
-) -> Result<ReturnCreateUser, sqlx::Error> {
-    match check_email(&pgpool, email).await {
-        Ok(true) => return Err(sqlx::Error::RowNotFound),
-        Ok(false) => (),
-        Err(error) => return Err(error),
-    };
-    let query_string = "INSERT INTO users (username,email,password) VALUES ($1,$2,$3) RETURNING *;";
-    match sqlx::query_as::<_, ReturnCreateUser>(query_string)
+pub async fn insert_default_user(
+    pgpool: &AppState) -> Result<(), sqlx::Error> {
+    let (username,password) = (Env::get_default_admin(),Env::get_default_admin_password());
+    match check_username(pgpool, username.as_str()).await {
+            Ok(true) => {return Ok(())},
+            Ok(false) => (),
+            Err(error) => return Err(error),
+        };
+    
+    let password = argon2_enc(&password).unwrap();
+    let query_string = "INSERT INTO users (username,password,admin) VALUES ($1,$2,$3);";
+    match sqlx::query(query_string)
         .bind(username.to_string())
-        .bind(email.to_string())
         .bind(password.to_string())
+        .bind(true)
         .fetch_one(&pgpool.db)
-        .await
-    {
-        Ok(new_user) => return Ok(new_user),
-        Err(e) => Err(e),
-    }
+        .await{
+            Ok(_) => Ok(()),
+            Err(error) => {match error{
+                sqlx::Error::RowNotFound => {return Ok(())},
+                _ => {return Err(error)}
+            }} 
+        }
+    
+}
+
+pub async fn retrieve_admin(pgpool: &Data<AppState>, username: &str) -> Result<bool, sqlx::Error> {
+    let query_string: &str = "SELECT admin FROM users WHERE username = $1 AND deleted_at IS NULL;";
+    let result = sqlx::query(query_string)
+        .bind(username.to_string())
+        .fetch_optional(&pgpool.db)
+        .await?;
+    Ok(result.is_some())
 }
