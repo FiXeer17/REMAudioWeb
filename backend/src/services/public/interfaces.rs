8ce093ccb8a3db83fca453154c67af88addb25eb
I3elle <supertizionico@gmail.com>
2025-04-08 17:03:35 +0200

    feat: retreive and add channels functions

--- backend/src/services/public/interfaces.rs
+++ backend/src/services/public/interfaces.rs
@@ -1,10 +1,12 @@
-use crate::utils::hasher::argon2_enc;
-use crate::{utils::configs::DatabaseEnv,services::public::signin::schemas::ReturnFullUser};
+use super::utils::{insert_user, Channel, SRC};
+use crate::utils::configs::ChannelsEnv;
 use crate::AppState;
+use crate::{services::public::signin::schemas::ReturnFullUser, utils::configs::DatabaseEnv};
 use actix_web::web::Data;
 
 pub async fn check_username(pgpool: &AppState, username: &str) -> Result<bool, sqlx::Error> {
-    let query_string: &str = "SELECT username FROM users WHERE username = $1 AND deleted_at IS NULL;";
+    let query_string: &str =
+        "SELECT username FROM users WHERE username = $1 AND deleted_at IS NULL;";
     let result = sqlx::query(query_string)
         .bind(username.to_string())
         .fetch_optional(&pgpool.db)
@@ -40,30 +42,14 @@ pub async fn from_id(pgpool: &Data<AppState>, id: i32) -> Result<ReturnFullUser,
     }
 }
 
-pub async fn insert_default_user(
-    pgpool: &AppState) -> Result<(), sqlx::Error> {
-    let (username,password) = (DatabaseEnv::get_default_admin(),DatabaseEnv::get_default_admin_password());
-    match check_username(pgpool, username.as_str()).await {
-            Ok(true) => {return Ok(())},
-            Ok(false) => (),
-            Err(error) => return Err(error),
-        };
-    
-    let password = argon2_enc(&password).unwrap();
-    let query_string = "INSERT INTO users (username,password,admin) VALUES ($1,$2,$3);";
-    match sqlx::query(query_string)
-        .bind(username.to_string())
-        .bind(password.to_string())
-        .bind(true)
-        .fetch_one(&pgpool.db)
-        .await{
-            Ok(_) => Ok(()),
-            Err(error) => {match error{
-                sqlx::Error::RowNotFound => {return Ok(())},
-                _ => {return Err(error)}
-            }} 
-        }
-    
+pub async fn insert_default_user(pgpool: &AppState) -> Result<(), sqlx::Error> {
+    let default_admin = DatabaseEnv::get_default_admin();
+    let default_admin_password = DatabaseEnv::get_default_admin_password();
+    let default_user = DatabaseEnv::get_default_user();
+    let default_user_password = DatabaseEnv::get_default_user_password();
+    insert_user(default_admin, default_admin_password, pgpool).await?;
+    insert_user(default_user, default_user_password, pgpool).await?;
+    Ok(())
 }
 
 pub async fn retrieve_admin(pgpool: &Data<AppState>, username: &str) -> Result<bool, sqlx::Error> {
@@ -73,4 +59,50 @@ pub async fn retrieve_admin(pgpool: &Data<AppState>, username: &str) -> Result<b
         .fetch_optional(&pgpool.db)
         .await?;
     Ok(result.is_some())
+}
+
+pub async fn retrieve_channels(
+    pgpool: &AppState,
+    user_id: i32,
+    src: SRC,
+) -> Result<Option<Vec<Channel>>, sqlx::Error> {
+    let query_string: &str = "SELECT * FROM channels WHERE user_id = $1 AND src=$2;";
+    let channels: Vec<Channel> = sqlx::query_as::<_, Channel>(query_string)
+        .bind(user_id)
+        .bind(src.to_string())
+        .fetch_all(&pgpool.db)
+        .await?;
+
+    if channels.is_empty() {
+        return Ok(None);
+    }
+    return Ok(Some(channels));
+}
+
+pub async fn add_channel(pgpool: &AppState, user_id: i32) -> Result<(),sqlx::Error>{
+    let query_string: &str ="INSERT INTO channels (channel_name,visible,src,user_id) VALUES ($1,$2,$3,$4);";
+
+    let (i_channels, o_channels, default_visibility, channel_prefix) = (
+        ChannelsEnv::get_i_channel_number(),
+        ChannelsEnv::get_o_channel_number(),
+        ChannelsEnv::get_default_visibility(),
+        ChannelsEnv::get_channel_default_prefix(),
+    );
+    
+    for i in 1..i_channels{
+        sqlx::query(query_string)
+        .bind(format!("{}{}",channel_prefix,i))
+        .bind(default_visibility)
+        .bind(SRC::INPUT.to_string())
+        .bind(user_id).fetch_optional(&pgpool.db).await?;
+    }
+    for i in 1..o_channels{
+        sqlx::query(query_string)
+        .bind(format!("{}{}",channel_prefix,i))
+        .bind(default_visibility)
+        .bind(SRC::OUTPUT.to_string())
+        .bind(user_id).fetch_optional(&pgpool.db).await?;
+    }
+
+    Ok(())
 }
---
+++ backend/src/services/public/utils.rs
@@ -0,0 +1,75 @@
+use serde::{Deserialize, Serialize};
+use sqlx::prelude::FromRow;
+
+use crate::{utils::hasher::argon2_enc, AppState};
+
+use super::interfaces::{check_username, retrieve_channels};
+
+#[derive(Deserialize, Serialize, Debug, FromRow)]
+pub struct Channel {
+    pub id: i32,
+    pub channel_name: String,
+    pub visible: bool,
+    pub user_id: i32,
+}
+
+pub async fn insert_user(
+    username: String,
+    password: String,
+    pgpool: &AppState,
+) -> Result<(), sqlx::Error> {
+    let (user, password) = (username, password);
+    match check_username(pgpool, &user).await {
+        Ok(true) => return Ok(()),
+        Ok(false) => (),
+        Err(error) => return Err(error),
+    };
+
+    let password = argon2_enc(&password).unwrap();
+    let query_string = "INSERT INTO users (username,password,admin) VALUES ($1,$2,$3);";
+    let query = sqlx::query(query_string)
+        .bind(user)
+        .bind(password.to_string())
+        .bind(true)
+        .fetch_optional(&pgpool.db)
+        .await;
+    match query {
+        Ok(_) => (),
+        Err(sqlx::Error::RowNotFound) => (),
+        Err(e) => return Err(e),
+    };
+    Ok(())
+}
+
+pub enum SRC {
+    INPUT,
+    OUTPUT,
+}
+impl ToString for SRC {
+    fn to_string(&self) -> String {
+        match self {
+            SRC::INPUT => String::from("input"),
+            SRC::OUTPUT => String::from("output"),
+        }
+    }
+}
+
+pub async fn retrieve_all_channels(
+    user_id: i32,
+    pgpool: &AppState,
+) -> Result<Option<Vec<Channel>>, sqlx::Error> {
+    let mut results: Vec<Channel> = Vec::new();
+    results.extend(
+        retrieve_channels(pgpool, user_id, SRC::INPUT)
+            .await?
+            .unwrap()
+            .into_iter(),
+    );
+    results.extend(
+        retrieve_channels(pgpool, user_id, SRC::OUTPUT)
+            .await?
+            .unwrap()
+            .into_iter(),
+    );
+    Ok(Some(results))
+}
