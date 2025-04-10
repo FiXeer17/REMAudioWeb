eae1962b7ff3f547922138a6f3730cecc8e4f1b2
I3elle <supertizionico@gmail.com>
2025-04-10 19:58:24 +0200

    feat: latest connection message

--- backend/src/services/public/root/routes.rs
+++ backend/src/services/public/root/routes.rs
@@ -2,7 +2,7 @@ use actix_web::{
     get, web::{self}, HttpRequest, HttpResponse, Responder
 };
 
-use crate::services::{private::app::{messages::GetConnections, tcp_manager::tcp_manager::TcpStreamsManager} , public::root::schemas::ReturnSockets};
+use crate::{services::{private::app::{messages::{GetConnections, GetLatestConnection}, tcp_manager::tcp_manager::TcpStreamsManager} , public::root::schemas::ReturnSockets}, utils::common::return_json_reason};
 
 #[get("")]
 pub async fn root(
@@ -12,8 +12,11 @@ pub async fn root(
     let message = GetConnections{};
     match srv.send(message).await{
         Ok(sockets) =>{
-            
-            let return_mess =ReturnSockets::new(sockets);
+            let latest_socket = srv.send(GetLatestConnection{}).await;
+            if let Err(_) = latest_socket{
+                return HttpResponse::InternalServerError().json(return_json_reason("cannot read latest connection."));
+            } 
+            let return_mess =ReturnSockets::new(sockets,latest_socket.unwrap());
             HttpResponse::Ok().json(return_mess)
         },
         Err(_) => HttpResponse::InternalServerError().finish()
