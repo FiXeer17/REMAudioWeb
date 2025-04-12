6a21694742adf72bcc2800f5925ce03eafd412c7
I3elle <supertizionico@gmail.com>
2025-04-12 13:58:10 +0200

    fix: no leatest_socket in sockets

--- backend/src/services/public/root/schemas.rs
+++ backend/src/services/public/root/schemas.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, net::SocketAddrV4};
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
-pub struct NameIpPort{
+pub struct NameIpPort {
     pub name: String,
     pub ip: String,
     pub port: String,
@@ -11,24 +11,28 @@ pub struct NameIpPort{
 #[derive(Deserialize, Serialize)]
 pub struct ReturnSockets {
     pub sockets: Option<Vec<NameIpPort>>,
-    pub latest_socket: Option<NameIpPort>
+    pub latest_socket: Option<NameIpPort>,
 }
 impl ReturnSockets {
     pub fn new(
         sockets: Option<HashMap<SocketAddrV4, String>>,
         latest_socket: Option<HashMap<SocketAddrV4, String>>,
     ) -> Self {
-        let latest_socket = latest_socket
-            .as_ref()
-            .and_then(|map| {
-                map.iter().next().map(|(socket, name)| NameIpPort {
-                    name: name.clone(),
-                    ip: socket.ip().to_string(),
-                    port: socket.port().to_string(),
-                })
-            });
+        let mut sockets_without_latest = sockets;
+        if sockets_without_latest.is_some() && sockets_without_latest.is_some() {
+            sockets_without_latest.as_mut()
+                .unwrap()
+                .remove(latest_socket.clone().unwrap().iter().next().unwrap().0);
+        }
+        let latest_socket = latest_socket.as_ref().and_then(|map| {
+            map.iter().next().map(|(socket, name)| NameIpPort {
+                name: name.clone(),
+                ip: socket.ip().to_string(),
+                port: socket.port().to_string(),
+            })
+        });
 
-        let sockets = sockets.map(|map| {
+        let sockets = sockets_without_latest.map(|map| {
             map.into_iter()
                 .map(|(socket, name)| NameIpPort {
                     name,
@@ -44,4 +48,3 @@ impl ReturnSockets {
         }
     }
 }
-
